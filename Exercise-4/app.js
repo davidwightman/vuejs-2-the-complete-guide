44522a6d8112dad9aabf0a0d18d7ec9a6428005b
David Wightman <david@davidwightman.org>
2019-03-24 14:29:00 -0400

    working on exercise 4

--- Exercise-4/app.js
+++ Exercise-4/app.js
@@ -1,11 +1,16 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    classValue: true,
+    color: 'green'
   },
   methods: {
     startEffect: function() {
-    
+      console.log(`hello`)
+      let vm = this;
+      setInterval(function(){
+        vm.classValue = !vm.classValue
+      }, 2000)
     }
   }
 });
