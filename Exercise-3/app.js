e2facae21e26af2e376830e73309b8e949aba63a
David Wightman <david@davidwightman.org>
2019-03-24 13:19:28 -0400

    2nd part of exercise 3 working

--- Exercise-3/app.js
+++ Exercise-3/app.js
@@ -2,11 +2,19 @@ new Vue({
         el: '#exercise',
         data: {
             value: 0,
-            result: ''
         },
         computed: {
-            hello: function(){
+            result: function(){
                 return this.value === 7 ? 'done' : 'not there yet'
+            }
+        },
+        watch: {
+            result: function(){
+                let vm = this;
+                setTimeout(function(){
+                    console.log(`hello`)
+                    vm.value = 0; 
+                }, 5000)
             }
         }
     });
