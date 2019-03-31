958be338bf3e44fe0616e9f3a587fcac1fc19d56
David Wightman <david@davidwightman.org>
2019-03-31 16:42:17 -0400

    finished Exercise 6 by adding Footer, ServerDetails, and ServerList components

--- Exercise-6/src/App.vue
+++ Exercise-6/src/App.vue
@@ -1,44 +1,22 @@
 <template>
     <div class="container">
-        <!-- <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
-        <hr> -->
         <Header />
         <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
+            <ServerList />
+            <ServerDetails />
         </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+        <Footer />
     </div>
 </template>
 
 <script>
 import Header from './Header.vue'
+import Footer from './Footer.vue'
+import ServerList from './ServerList.vue'
+import ServerDetails from './ServerDetails.vue'
 
 export default {
-    components: {Header},
+    components: {Header, Footer, ServerList, ServerDetails},
 }
 </script>
 
