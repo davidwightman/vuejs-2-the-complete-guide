27e9ca02e3a217ad32bf80cd1a50d1246fb06e20
David Wightman <david@davidwightman.org>
2019-04-01 22:14:20 -0400

    started working on Exercise 7

---
+++ Exercise-7/src/components/Server/ServerDetails.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p>Server {{id}} status is {{status}}</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: ['id', 'status']
+}
+</script>
+
+<style>
+
+</style>
---
+++ Exercise-7/src/components/Server/Servers.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li class="list-group-item"
+                v-for="item in servers"
+                @click="getServerDetail(event)"
+                :currnet-server.id = "id"
+                :currnet-server.status = "status"
+            >
+                    Server #{{ item.id }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+         return{
+            servers: [
+                {id: 1, status: 'normal'},
+                {id: 2, status: 'critical'},
+                {id: 3, status: 'normal'},
+                {id: 4, status: 'unknown'},
+                {id: 5, status: 'unknown'},
+            ],
+            currentServer: {id: 1, status: 'normal'}
+         }
+    },
+    methods: {
+        getServerDetail(event) {
+            // console.log(`clicked`)
+            console.log(event)
+            this.currentServer = this.servers[event.target.value]
+            console.log(this.currentServer)
+        }
+    }
+   
+}
+</script>
+
+<style>
+
+</style>
